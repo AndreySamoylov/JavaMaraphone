429c8394965504743cbf8b45710dc6d7d50e06e0
AndreySamoylov <64429941+AndreySamoylov@users.noreply.github.com>
2021-04-05 10:47:31 +0500

    Add files via upload

---
+++ JavaMaraphon/src/Day1/Task6.java
@@ -0,0 +1,12 @@
+package Day1;
+
+//Таблица умножения для какого то числа
+public class Task6 {
+    public static void main(String[] args) {
+        int k = 9;
+
+        for (int i = 1; i < 10; i++){
+            System.out.println(i + " * " + k + " = " + i * k);
+        }
+    }
+}
