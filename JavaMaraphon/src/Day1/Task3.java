a323ed6340f89cac9dad83f94a2e2a1b28deb844
Andrey Samoylov <samoylovanandreial@gmail.com>
2021-04-05 12:08:43 +0500

    New

--- JavaMaraphon/src/Day1/Task3.java
+++
@@ -1,9 +0,0 @@
-package Day1;
-
-public class Task3 {
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++){
-            System.out.println("Java");
-        }
-    }
-}
---
+++ src/Day1/Task5.java
@@ -0,0 +1,10 @@
+package Day1;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int year = 1980;
+        for (int i = year; i <=2020; i+=4) {
+            System.out.println("Олимпиада " + year + " года");
+        }
+    }
+}
